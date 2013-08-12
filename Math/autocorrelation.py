e5ca1fe9fb2ae90be4a020260c5888eb374801a8
lyh <lyhopq@gmail.com>
2013-08-12 23:01:15 +0800

    update

--- Math/autocorrelation.py
+++ Math/autocorrelation.py
@@ -5,7 +5,8 @@ import numpy as np
 #import matplotlib
 #zhfont1 = matplotlib.font_manager.FontProperties(fname='/home/lyh/.fonts/simsun.ttc')
 
-x = np.arange(120.0)
+t = np.arange(0, 20*np.pi, 0.1)
+x = np.sin(t)
 N = len(x)
 mu = 1.0/N*sum(x)
 
@@ -14,7 +15,8 @@ for k in range(N):
     c[k] = sum((x[:N-k]-mu)*(x[k:]-mu))/sum((x-mu)**2)
 
 plt.figure(figsize=(8,3))
-plt.plot(x, c)
+plt.plot(t, c, label=u'$sin(x)$ auto correlation')
 #plt.ylabel(u'Correlation相关函数', fontproperties=zhfont1)
-plt.ylabel(u'Correlation')
-plt.savefig('correlation.png')
+plt.legend()
+plt.savefig('autocorrelation.png')
+
